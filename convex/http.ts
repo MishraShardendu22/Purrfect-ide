fbd248ec45db54da8f62ba52c996c138e2ee2404
Shardendu Mishra <shardendumishrabrave@gmail.com>
2024-12-19 22:56:23 +0530

    User Schema Created and Webhook to COnvex Made and Initialised

---
+++ convex/http.ts
@@ -0,0 +1,66 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Webhook } from "svix";
+import { httpAction } from "./_generated/server";
+import { httpRouter } from "convex/server";
+import { WebhookEvent } from "@clerk/nextjs/server";
+import { api, internal } from "./_generated/api";
+
+const http = httpRouter();
+
+http.route({
+    path: "/clerk-webhook",
+    method: "POST",
+    handler: httpAction(async (ctx, req) => {
+        const webhookSecret = process.env.CLERK_WEBHOOK_SECRET;
+        if (!webhookSecret) {
+            return new Response("Webhook secret not configured", { status: 500 });
+        }
+
+        const svix_id = req.headers.get("svix-id");
+        const svix_signature = req.headers.get("svix-signature");
+        const svix_timestamp = req.headers.get("svix-timestamp");
+
+        if (!svix_id || !svix_signature || !svix_timestamp) {
+            return new Response("Missing ID, signature, or timestamp", { status: 400 });
+        }
+
+        const data = await req.json();
+        const body = JSON.stringify(data);
+
+        const wh = new Webhook(webhookSecret);
+        let evt: WebhookEvent;
+
+        try {
+            evt = wh.verify(body, {
+                "svix-id": svix_id,
+                "svix-signature": svix_signature,
+                "svix-timestamp": svix_timestamp,
+            }) as WebhookEvent;
+        } catch (error) {
+            console.error("Error Verifying Webhook", error);
+            return new Response("Invalid signature", { status: 400 });
+        }
+
+        const eventType = evt.type;
+        if (eventType === "user.created") {
+            const { id, email_addresses, first_name, last_name } = evt.data;
+            const email = email_addresses[0].email_address;
+            const name = `${first_name || ""} ${last_name || ""}`.trim();
+
+            try {
+                await ctx.runMutation(api.users.syncUser, {
+                    userId: id,
+                    email,
+                    name,
+                });
+            } catch (error) {
+                console.log("Error creating user:", error);
+                return new Response("Error creating user", { status: 500 });
+            }
+        }
+
+        return new Response("Web-Hook Made and Data Saved in Database", { status: 200 });
+    }),
+});
+
+export default http;
