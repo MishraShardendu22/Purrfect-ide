157084d94aaaabc9dcd0a12c6713b87ab1fed55b
Shardendu Mishra <shardendumishrabrave@gmail.com>
2024-12-19 19:31:37 +0530

    Connecting Clerk Authentication and Convex Database

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const config = {
+    providers: [
+      {
+        domain: "https://patient-fox-50.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+export default config;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,29 @@
+// import { SignedIn, SignedOut, SignOutButton, SignUpButton, UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      <h1>hello world
-      </h1>
-    </div>
+    <>
+    
+    </>
   )
 }
 
 export default page
+
+
+// <div>
+// <SignedOut>
+//   <SignUpButton>
+
+//   </SignUpButton>
+// </SignedOut>
+
+// <UserButton />
+
+// <SignedIn>
+//   <SignOutButton>
+
+//   </SignOutButton>
+// </SignedIn>
+// </div>
